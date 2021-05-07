7219960433ad61edfe2dba71b2d55f6534f3dffb
gabycorfield <61152817+gabycorfield@users.noreply.github.com>
2021-05-07 09:01:24 -0300

    Update cabecera.php

--- parciales/cabecera.php
+++ parciales/cabecera.php
@@ -21,7 +21,8 @@
 <!-- Latest compiled and minified JavaScript -->
 <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script> 
 <script type="text/javascript" src="js/jquery.datetimepicker.full.min.js"></script>
-<script type="text/javascript" src="jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>    
+<!--<script type="text/javascript" src="jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>-->
 <!-- Custom JS file -->
 <script type="text/javascript" src="js/codigoGral.js"></script>
 <script type="text/javascript" src="js/codigoAjax.js"></script>
